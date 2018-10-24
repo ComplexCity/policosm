06046df2dc2c6b99fe7f13dee97f51fc86297bcb
alextheivp <40376446+alextheivp@users.noreply.github.com>
2018-10-24 10:15:45 +0200

    Add files via upload

--- policosm/classes/transportation.py
+++ policosm/classes/transportation.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 '''
-Created in February 2017 in ComplexCity Lab
+In progress since June 2018 in ComplexCity Lab
 
 @author: github.com/fpfaende
 
-what it does
-	Roads sets callback functions for ways in osm files targeting roads
+Object :
+	The transportation module which transforms xml data into mobility graph
 
-parameters
+Parameters :
+	OSM File (xml required, pbf not ready yet)
+	[Functions with POLICOSM SAX PARSER (see policosmsax.py)]
 
-how it works
+How it works :
+	N/A
 
-return
+Returns :
+	A Transportation object which graph can be called
 
 '''
-
-import networkx as nx
-
+import os
 import sys
-sys.path.insert(0, '/Users/fabien/workspace/github/policosm')
+import json
+import hashlib
+import geojson
+import networkx as nx
+import matplotlib.pyplot as plt
 
-# TODO 
+from shapely.geometry import LineString, Point
+
+sys.path.insert(0, '/home/alex/Bureau/policosm')
+from geoNetworks.linkNewNodes 	import linkNode_OSM
+import functions.transportationLineAnalysis as tla 
 
 class Transportation(object):
 	def __init__(self):
@@ -29,105 +39,299 @@ class Transportation(object):
 		self.nodes = {}
 		self.ways = {}
 		self.relations = {}
+		self.to_eulerian = {}
+		self.graph = nx.MultiDiGraph()
+		self.r_graph = nx.MultiDiGraph()
+		self.geolexical = {}
+		self.nodes_residue = 0
+		self.stations = {}
+		self.boundary = nx.MultiDiGraph()
+	
+	def getGraph(self):
+		return self.graph
 
-	def osmcoordinates(self, coords):
-		for osmid, lon, lat in coords:
-			self.coords[osmid] = (lon, lat)
+	def get_Roads(self):
+		return self.r_graph
 
-	def osmnodes(self, nodes):
-		for osmid, tags, coords in nodes:
-			name = tags['name'] if 'name' in tags else None
-			highway = tags['highway'] if 'highway' in tags else None
-			bus = tags['bus'] if 'bus' in tags else None
-			public_transport = tags['public_transport'] if 'public_transport' in tags else None
-			network = tags['network'] if 'network' in tags else None 
-			operator = tags['operator'] if 'operator' in tags else None
-			self.nodes[osmid]=(coords, name, highway, bus, public_transport, network, operator)
+	def get_Bounds(self):
+		return self.boundary
 
-	def osmways(self, ways):
-		for osmid, tags, refs in ways:
-			#tag 
-			self.ways[osmid] = (refs, tags)
+	def draw_roads(self):
+		print "ROADS DRAWING"
+		c , t , p = 1, len(self.ways), 0.25
+		for key, value in self.ways.items():
+			if (float(c)/float(t)) > p or c==t-1 :
+				print p*100, "%", " complete"
+				p+=0.25
 
-	def osmrelations(self, relations):
-		for osmid, tags, members in relations:
-			# tag name network operator ref route_master type (route_master) wheelchair
-			# tag name network operator ref route type (route) wheelchair
-			if 'route' in tags['type']:
-				self.relations[osmid] = (tags, members)
+			if 'highway' in value['tags']:
+				R = tla.road_support(key, value['tags'], value['nds'],self.nodes,self.r_graph)
+				self.r_graph = R[0]
+				c+=1
+			else :
+				c+=1
+				continue
+
+	def draw_bounds(self) :
+
+		count = 0
+
+		for keyOsmid, relValues in self.relations.items() :
+			
+			tags, members = relValues['tags'], relValues['membs']
+
+			if 'admin_level' in tags and tags['admin_level'] == '8' :
+
+				ways_in_relation = {}
+
+				for elem in range (len(members)):
+					osmid, osmType,osmDetail = members[elem]['ref'], members[elem]['type'], members[elem]['role']
+
+					if osmType == 'way':
+						if osmid in self.ways : 
+							way_nodes = self.ways[osmid]['nds']
+							way_tag = self.ways[osmid]['tags']
+
+							ways_in_relation[osmid] = {'nds' : way_nodes, 'tags' : way_tag}
+							
+				for key,value in ways_in_relation.items() :
+
+					tags = value['tags']
+					nds = value['nds']
+
+					list_of_nodes = nds
+					constructible = []
+					for nodei in list_of_nodes :
+
+						the_node = int(nodei)
+						lon, lat = self.nodes[the_node]['coords'][0], self.nodes[the_node]['coords'][1]
+						aff = int(hashlib.md5(str(lon) + str(lat) + str(the_node) + "BOUND NUMBER" + str(count)).hexdigest(), 16)
+						if aff not in list(self.boundary.nodes) :
+							self.boundary.add_node(aff,original_osmid = str(the_node), longitude=lon, latitude=lat, utilisation="Boundary", color = '#000000')
+						constructible.append(aff)
+
+						if the_node == 421488092 :
+							print "HERE", aff
+					#print constructible
+					for nodei in range (1,len(constructible)) :
+						self.boundary.add_edge( int(constructible[nodei-1]), int(constructible[nodei]), special='Boundary')
+
+					count += 1
+
+			else :
+				continue
+
 
 	def info(self):
 		info =  'nodes:' + str(len(self.nodes)) + '\n'
 		info += 'ways:' + str(len(self.ways)) + '\n'
 		info += 'relations:' + str(len(self.relations)) + '\n'
 		return info
+	
+	def transportation_Analysis (self, lineAnalysis = False, extern_Graph = None):
 
-class Bus(Transportation):
-	def __init__(self, coords={}, nodes={}, ways={}, relations={}):
-		if nodes is None and ways is None and relations is None:
-			super(Transportation, self).__init__()
-		else:
-			self.coords = coords
-			self.nodes = nodes
-			self.ways = ways
-			self.relations = relations
+		if extern_Graph != None :
+			self.graph = extern_Graph
+	
+		fichierCSV = open('data1.csv','w')
+		csvData = open('data2.csv','w')
 
-	def analyseBus(self):
-		print self.nodes.keys()
-		relations = {}
-		for keyOsmid, (tags, members) in self.relations.iteritems():
-			if 'route' in tags:
-				if tags['route'] <> 'bus':
+		isDiGraph = nx.DiGraph()
+		noDiGraph = nx.Graph()
+		default_counter = 1
+		fichier = open('notes.txt','w')
+		nb_relations, relations_count = len(self.relations),1
+
+		means = ['bus','subway','tram','light_rail']
+		list_of_diGraphs = []
+
+		for keyOsmid, relValues in self.relations.items():
+
+			if relations_count < 0 :
+				print "RELATION NUMBER ", relations_count, "SKIPPED"
+				relations_count += 1
+				continue
+
+			tags, members = relValues['tags'], relValues['membs']
+
+			print "PROGRESS : ", relations_count, "/", nb_relations
+			# STEP 1 : Determine if the relation is linked to a transportation system and draw the line
+			if (('route' in tags) and (tags['route'] in means)) or (('route_master' in tags) and (tags['route_master'] in means)) or (('type' in tags) and ('public_transport' in tags)):
+				try :
+					mean = tags['route']
+				except :
+					try :
+						mean = tags['route_master']
+					except :
+						mean = 'stop_area'
+
+				G = tla.elements_du_graphe(self.graph,isDiGraph,noDiGraph,keyOsmid,tags,members, self.nodes, self.ways, self.coords, default_counter, fichierCSV, csvData, self.stations, self.nodes_residue, type_tr = mean)
+
+			else :
+				if 'route' in tags:
+					print "The algorithm doesn't process this route : ", tags['route']
+				else :
+					print "The algorithm doesn't process this relation : ", tags
+				print '\n'
+				relations_count += 1
+				continue
+
+			if type(G) is int :
+				relations_count += 1
+				continue
+			else :
+				if len(G) == 1 : # CASE OF STOP AREA
+					self.graph = G[0]
+					relations_count += 1
+					print '\n'
 					continue
-			if 'route_master' in tags:
-				if tags['route_master'] <> 'bus':
-					continue
-			#prepare ways
-			relations[keyOsmid] = (tags, members)
-			print tags
-			for osmid, osmType, osmDetail in members:
-				if osmType == 'node':
-					try:
-						if osmid in self.nodes:
-							print 'NODE',self.nodes[osmid], osmDetail
-						else:
-							print 'NODE ######',self.coords[osmid],osmDetail
-					except Exception, e:
-						print 'NODE out of town'
-				elif osmType == 'way':
-					try:
-						print 'WAY', self.ways[osmid], osmDetail
-					except Exception, e:
-						print 'WAY out of town'
+				else :
+					self.graph, isDiGraph, noDiGraph, ways_in_relation, relation_station, relation_platforms, info_name, default_counter, self.stations, self.nodes_residue = G[0], G[1], G[2], G[3], G[4], G[5], G[6], G[7], G[8], G[9]
+					print "STEP 1 COMPLETE , FINAL GRAPH STATE : ", len( list (self.graph.edges)), " EDGES ; ", len( list (self.graph.nodes)), " NODES "
 
-			# TODO RECREATE THE GRAPH WITH A GRAPH FOR EACH LINE ADDING STOPS
+			# STEP 2 : ORIENTING THE GRAPH
+				# On essaye dans un premier temps de déterminer les terminus grâce au nom de ligne
+			R = tla.analyse_lexicale(self.graph,noDiGraph,isDiGraph,info_name,relation_station,fichier, tags, type_mobility=mean)
+			self.graph, noDiGraph, isDiGraph, res, departure, arrival = R[0], R[1], R[2], R[3], R[4], R[5]
+
+			if res == 0 :
+				R = tla.analyse_lexicogeographique(self.graph,noDiGraph,isDiGraph,info_name,relation_station,fichier, ways_in_relation, departure, arrival, type_mobility = mean)
+				self.graph, noDiGraph, isDiGraph, res = R[0], R[1], R[2], R[3]
+
+				if res == 0 : 
+					R = tla.redirection_par_defaut(self.graph,noDiGraph,isDiGraph,info_name,relation_station,fichier, ways_in_relation, type_mobility = mean)
+
+					if type(R)== int :
+						self.to_eulerian[info_name] = relation_station
+						relations_count += 1
+						continue
+					else :
+						self.graph, noDiGraph, isDiGraph = R[0], R[1], R[2]
+					#fichierCSV.write(info_name.encode('utf-8') + ";" + str("Default") + "\n")
+				else :
+					#fichierCSV.write(info_name.encode('utf-8') + ";" + str("Level 2") + "\n")
+					pass
+			else :
+				#fichierCSV.write(info_name.encode('utf-8') + ";" + str("Level 1") + "\n")
+				pass
+
+			# Tracé des cycles 
+			R = tla.trace_cycle(self.graph,noDiGraph,isDiGraph,info_name,relation_station,fichier, type_mobility=mean)
+			self.graph, noDiGraph, isDiGraph = R[0], R[1], R[2]
+			
+			print "STEP 2 COMPLETE , FINAL GRAPH STATE : ", len( list (self.graph.edges)), " EDGES ; ", len( list (self.graph.nodes)), " NODES "
+			
+			# ETAPE 3a : On rajoute les plateformes : simplifiés en noeud centroïde [exclusif bus pour l'instant]
+				#Rq : mise à la fin pour éviter les conflits de degré de noeuds			
+			if mean in ['bus','light_rail','tram']:
+				utiliZation = "Bus Station" if mean == 'bus' else "Tramway Station" if mean == 'tram' else "Light Rail Station" if mean == 'light_rail' else "Undfined Station"
+				for key,value in relation_platforms.items() :
+					list_of_nodes = value[0]
+					longs, latts = [], []
+					for nodei in range (len(list_of_nodes)) :
+						longs.append(self.nodes[list_of_nodes[nodei]]['coords'][0])
+						latts.append(self.nodes[list_of_nodes[nodei]]['coords'][1])
+					moy_longs = float(sum(longs)/len(longs))
+					moy_latts = float(sum(latts)/len(longs))
 					
-			self.relations = relations
+					s_point = Point(moy_longs,moy_latts)
+					adde  = -int(hashlib.md5(s_point.wkt).hexdigest(), 16)
+					isDiGraph.add_node(adde,longitude=moy_longs, latitude=moy_latts, nom = value[1] ,utilisation=utiliZation,ligne = info_name)
+					self.graph.add_node(adde,longitude=moy_longs, latitude=moy_latts, nom = value[1] ,utilisation=utiliZation,ligne = info_name)
 
-			#prepare nodes
-			# TODO IF DISTANCE IS TOO SHORT, MUST BE TWO SIDES OF THE ROAD ! Keep points ?
-			# NODE ON THE RIGHT OF THE WAY ONE DIRECTION
-			# NODE ON THE OTHER SIDE ANOTHER DIRECTION
+					nearestCouple = {}
+					edges = list(isDiGraph.edges)
+					for p in range (len(edges)):
+						linestring = geojson.LineString([(isDiGraph.nodes[edges[p][0]]['longitude'],isDiGraph.nodes[edges[p][0]]['latitude']) , (isDiGraph.nodes[edges[p][1]]['longitude'],isDiGraph.nodes[edges[p][1]]['latitude'])])
+						feature = geojson.Feature(geometry=linestring, properties={"node1": edges[p][0] ,"node2": edges[p][1]}) 
+						candidate = geojson.dumps(feature)
+						line = json.loads(candidate)
+						nearestCouple[s_point.distance(LineString(line['geometry']['coordinates']))]=(line['properties'].values())
+					if nearestCouple != {} :
+						proche_Edge = min(nearestCouple.keys())
+						#print proche_Edge
+						#print "A", nearestCouple[proche_Edge]
+						if proche_Edge < 0.0001 :
+							R = linkNode_OSM(isDiGraph,adde,nearestCouple[proche_Edge],diriged = True, bus = True)
+							isDiGraph, new_node, u,v = R[0], R[1], R[2], R[3]
+							self.graph.add_node(new_node, utilisation='Link Platform_BusLine',ligne = info_name)
+							new_node_attributes = {new_node : isDiGraph.nodes[new_node]}
+							#print new_node_attributes
+							nx.set_node_attributes(self.graph,new_node_attributes)
+							use = 'Platform_to_' + str(mean.capitalize())
+							waiting_time = tla.waiting_bus_time if mean == 'bus' else tla.waiting_trm_time if (mean == 'tram' or mean == 'light_rail') else tla.waiting_bus_time
+							isDiGraph.add_edge(adde,new_node, type_mobility=use, ligne= info_name, time_travel= waiting_time)		# Entry
+							isDiGraph.add_edge(new_node,adde, type_mobility=use, ligne= info_name, time_travel= 0) 					# Exit
+						else :
+							continue
+					else :
+						continue
 
+				print "STEP 3 COMPLETE , FINAL GRAPH STATE : ", len( list (self.graph.edges)), " EDGES ; ", len( list (self.graph.nodes)), " NODES "
 
-'''
-one master relation with some properties
+			# STEP 4 : On raccorde les stations intermdiaires représentées que par des points
+				#Rq : mise à la fin pour éviter les conflits de degré de noeuds
+			for key,value in relation_station.items() :
+				if isDiGraph.degree[key] == 0 :
+					node_to_link = key
+					s_point = Point([value['coords'][0],value['coords'][1]])
 
--> some relations for each way
+					nearestCouple = {}
+					edges = list(isDiGraph.edges)
+					for p in range (len(edges)):
+						#print edges[p][0], isDiGraph.nodes[edges[p][0]], edges[p][1], isDiGraph.nodes[edges[p][1]]
+						linestring = geojson.LineString([(isDiGraph.nodes[edges[p][0]]['longitude'],isDiGraph.nodes[edges[p][0]]['latitude']) , (isDiGraph.nodes[edges[p][1]]['longitude'],isDiGraph.nodes[edges[p][1]]['latitude'])])
+						feature = geojson.Feature(geometry=linestring, properties={"node1": edges[p][0] ,"node2": edges[p][1]}) 
+						candidate = geojson.dumps(feature)
+						line = json.loads(candidate)
+						nearestCouple[s_point.distance(LineString(line['geometry']['coordinates']))]=(line['properties'].values())
+					if nearestCouple != {} :
+						proche_Edge = min(nearestCouple.keys())
+						#print proche_Edge
+						#print "A", nearestCouple[proche_Edge]
+						if proche_Edge < 0.0001 :
+							R = linkNode_OSM(isDiGraph,node_to_link,nearestCouple[proche_Edge],diriged = True, bus = True)
+							isDiGraph, new_node, u, v = R[0], R[1], R[2], R[3]
+							self.graph.add_node(new_node, utilisation='Link Station_BusLine', ligne = info_name)
+							new_node_attributes = {new_node : isDiGraph.nodes[new_node]}
+							#print new_node_attributes
+							nx.set_node_attributes(self.graph,new_node_attributes)
+							if mean in ['bus','light_rail','tram'] :
+								use = 'Station_to_' + str(mean.capitalize())
+								waiting_time = tla.waiting_bus_time if mean == 'bus' else tla.waiting_trm_time if (mean == 'tram' or mean == 'light_rail') else tla.waiting_bus_time
+								isDiGraph.add_edge(node_to_link,new_node, type_mobility=use, ligne= info_name, time_travel= waiting_time)	# Entry
+								isDiGraph.add_edge(new_node,node_to_link, type_mobility=use, ligne= info_name, time_travel= 0)				# Exit
+							elif mean == 'subway' :
+								isDiGraph.add_edge(node_to_link,new_node, type_mobility='Subway_Linked', ligne= info_name, time_travel= tla.waiting_sub_time)
+								isDiGraph.add_edge(new_node,node_to_link, type_mobility='Subway_Linked', ligne= info_name, time_travel= 0)							
 
-each relations has nodes and ways 
+						else :
+							continue
+					else :
+						continue
+				else :
+					continue
 
-stops can be node or polygon
+			print "STEP 4 COMPLETE , FINAL GRAPH STATE : ", len( list (self.graph.edges)), " EDGES ; ", len( list (self.graph.nodes)), " NODES "
 
-'''
+			nx.set_node_attributes(isDiGraph,self.stations)
+			self.stations = {}
+
+			# STEP 4 : ADD INTO THE FINAL GRAPH
+			if mean == 'bus' :
+				self.graph = tla.replacer_A(isDiGraph, self.graph, info_name)
+			else :
+				self.graph = tla.replacer_B(isDiGraph, self.graph, info_name)
+			print "STEP 5 COMPLETE , FINAL GRAPH STATE : ", len( list (self.graph.edges)), " EDGES ; ", len( list (self.graph.nodes)), " NODES "
+
+			print '\n'
+
+			isDiGraph.clear()
+			noDiGraph.clear()
+			relations_count += 1
+
+		fichier.close()
+		fichierCSV.close()
 
 if __name__ == "__main__":
-	from imposm.parser import OSMParser
-	transportation = Transportation()
-	osmParser = OSMParser(concurrency=4, coords_callback=transportation.osmcoordinates, nodes_callback=transportation.osmnodes, ways_callback=transportation.osmways, relations_callback=transportation.osmrelations)
-	osmParser.parse('/Users/fabien/workspace/github/policosm/policosm/tests/01249.pbf')
-	print transportation.info()
-	bus = Bus(coords=transportation.coords, nodes=transportation.nodes, ways=transportation.ways, relations=transportation.relations)
-	bus.analyseBus()
-	print bus.info()
-
+	print "Rien"
